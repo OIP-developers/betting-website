8f1e1cf9173093b76aabd0f44ca25ccf548be373
Dev4644 <dev4@oip.com.pk>
2025-02-26 20:19:12 +0500

    error solved last

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
+'use client'
+
 import type { Metadata } from "next";
 import "./globals.css";
 import ClientLayout from "../components/ClientLayout";
 
-export const metadata: Metadata = {
-  title: "Luck Box",
-  description: "A trading box that generates consistent daily profits.",
-};
+// export const metadata: Metadata = {
+//   title: "Luck Box",
+//   description: "A trading box that generates consistent daily profits.",
+// };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
